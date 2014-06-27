1f130d31fc196b2df14dbdde7ccff35fec60a864
Justin R. Stout <justin.stout@perceptivesoftware.com>
2014-06-27 15:23:32 -0500

    Add recipe for installing power tools.

---
+++ recipes/install_tfpt.rb
@@ -0,0 +1,3 @@
+windows_package 'Microsoft Team Foundation Server 2010 Power Tools' do
+  source 'http://visualstudiogallery.msdn.microsoft.com/c255a1e4-04ba-4f68-8f4e-cd473d6b971f/file/35473/4/tfpt.msi'
+end
