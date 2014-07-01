e2594af0b0173d058ff268d43852b9c13defefd4
Justin R. Stout <justin.stout@perceptivesoftware.com>
2014-07-01 13:00:35 -0500

    Remove unused recipes, clean up attributes, fix helper.

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,7 +1,7 @@
 module Visualstudio
   module Helper
     def is_vs90_installed?()
-      key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\DevDiv\vs\Servicing\9.0\PRO\1033'
+      key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\DevDiv\vs\Servicing\9.0\IDE\1033'
       value = { name: 'Install', type: :dword, data: 1 }
       registry_key_exists?(key) and
       registry_value_exists?(key, value) and
@@ -9,7 +9,7 @@ module Visualstudio
     end
     
     def is_vs90sp1_installed?()
-      key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\DevDiv\vs\Servicing\9.0\PRO\1033'
+      key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\DevDiv\vs\Servicing\9.0\IDE\1033'
       value = { name: 'SP', type: :dword, data: 1 }
       registry_key_exists?(key) and
       registry_value_exists?(key, value) and
@@ -31,18 +31,5 @@ module Visualstudio
       registry_value_exists?(key, value) and
       registry_data_exists?(key, value)
     end
-
-    def is_vs_installed?()
-      File.exists?(File.join(node['visualstudio']['install_dir'], '\Common7\IDE\devenv.exe'))
-    end
-
-    def is_sp_installed?()
-      registry_key_exists?(node['visualstudio']['update']['package_regkey'])
-    end
-
-    def is_vsto_installed?()
-      registry_key_exists?(node['visualstudio']['vsto']['package_regkey'])
-    end
-
   end
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,5 @@
-#
-# Author:: Shawn Neal <sneal@daptiv.com>
-# Cookbook Name:: visualstudio
-# Recipe:: default
-#
-# Copyright 2013, Daptiv Solutions, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# VS installation requires .NET 4.5 to be installed first
-include_recipe 'visualstudio::dotnet_prereq'
-
-# We require 7-zip in order to extract the ISOs
-include_recipe 'seven_zip'
-
-# Enable/disable Nuget package restore
-include_recipe 'visualstudio::nuget'
-
-# Install VS
-include_recipe 'visualstudio::install'
+include_recipe 'visualstudio::install_vs90'
+include_recipe 'visualstudio::install_vs90sp1'
+include_recipe 'visualstudio::install_vs100'
+include_recipe 'visualstudio::install_vs100sp1'
+include_recipe 'visualstudio::install_tfpt'
